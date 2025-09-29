95f896d1df0f38ebd85bb5497e1b75f12aaef0bc
Mohamed Eid Latif <MohamedLatif@my.uopeople.edu>
2025-09-29 14:30:28 +0300

    feat: implement update todo

--- routes/todosRoutes.ts
+++ routes/todosRoutes.ts
@@ -50,9 +50,30 @@ router.post("/", async (req: Request, res: Response) => {
 });
 
 // update todo
-router.patch("/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.send("todo updated");
+router.patch("/:id", async (req: Request, res: Response) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["title", "description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const todo = await Todo.findOne({ _id: req.params.id, user: "60d5f9f8f8a8a0a8a0a8a0a8" }); // placeholder user id
+
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((update) => (todo[update] = req.body[update]));
+    await todo.save();
+    res.send(todo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 // delete todo
 router.delete("/:id", (req: Request, res: Response) => {
