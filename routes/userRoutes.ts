21db6fc00be5e71671eb14584eeb9194857cca81
Mohamed Eid Latif <MohamedLatif@my.uopeople.edu>
2025-09-29 14:23:29 +0300

    feat: implement user creation

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -6,9 +6,17 @@ router.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-router.post("/", (req: Request, res: Response) => {
-  res.send("User created");
-  console.log("User data:", req.body);
+const User = require("../models/userModel");
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 router.patch("/:id", (req: Request, res: Response) => {
