284a9f3e84e1564384263932bbde7047650005e5
Mohamed Eid Latif <MohamedLatif@my.uopeople.edu>
2025-09-29 14:26:52 +0300

    feat: implement update user

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -36,9 +36,30 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-router.patch("/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  res.send(`User with ID ${id} updated`);
+router.patch("/:id", async (req: Request, res: Response) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 router.delete("/:id", (req: Request, res: Response) => {
